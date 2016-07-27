604ee6d21a8fd2bbac2a17804189ed4fa904111d
Kenneth <matovken@gmial.com>
2016-07-27 17:06:25 +0300

    clean message

--- data_api/api/serializers.py
+++ data_api/api/serializers.py
@@ -166,22 +166,14 @@ class FlowReadSerializer(BaseDocumentSerializer):
 
 
 class MessageReadSerializer(BaseDocumentSerializer):
-    # broadcast = SerializerMethodField()
     contact = SerializerMethodField()
-    # labels = SerializerMethodField()
 
     class Meta:
         model = Message
-        exclude = ALWAYS_EXCLUDE + ('tid', 'urn', 'broadcast')
-
-    # def get_broadcast(self, obj):
-    #     return str(obj.broadcast.get('id', '')) or None
+        exclude = ALWAYS_EXCLUDE + ('tid', 'urn', 'broadcast', 'labels')
 
     def get_contact(self, obj):
         return str(obj.contact.get('id', '')) or None
-
-    # def get_labels(self, obj):
-    #     return [l['name'] for l in obj.labels]
 
 
 class BroadcastReadSerializer(BaseDocumentSerializer):
