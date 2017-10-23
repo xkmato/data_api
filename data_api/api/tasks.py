fbebed81549796ee37e961d6d344b3475d2f4959
Cory Zue <czue@dimagi.com>
2017-10-23 14:10:27 +0200

    tidy up fetch code a bit

--- data_api/api/tasks.py
+++ data_api/api/tasks.py
@@ -3,7 +3,8 @@ import traceback
 from django.conf import settings
 import requests
 from retrying import retry
-from temba.base import TembaAPIError, TembaConnectionError, TembaException, TembaPager
+from temba_client.exceptions import TembaConnectionError, TembaBadRequestError, TembaTokenError, \
+    TembaRateExceededError, TembaException
 from data_api.api.models import BaseUtil, Org, Message, Run
 from djcelery_transactions import task
 
@@ -14,30 +15,26 @@ logger = logging.getLogger("tasks")
 
 
 def retry_if_temba_api_or_connection_error(exception):
-    if isinstance(exception, TembaAPIError) and isinstance(exception.caused_by,
-                                                           requests.HTTPError
-                                                           ) and 399 < exception.caused_by.response.status_code < 500:
+    if isinstance(exception, (TembaBadRequestError, TembaTokenError)):
         return False
-    if isinstance(exception, TembaAPIError) or isinstance(exception, TembaConnectionError):
+    if isinstance(exception, (TembaConnectionError, TembaRateExceededError)):
         logger.warning("Raised an exception: %s - Retrying in %s minutes", str(exception),
-                   str(settings.RETRY_WAIT_FIXED/60000))
+                       str(settings.RETRY_WAIT_FIXED/60000))
         return True
     return False
 
 
 @retry(retry_on_exception=retry_if_temba_api_or_connection_error, stop_max_attempt_number=settings.RETRY_MAX_ATTEMPTS,
        wait_fixed=settings.RETRY_WAIT_FIXED)
-def fetch_entity(entity, org, n):
-    entity = entity.get('name')
-    logger.info("Fetching Object of type: %s for Org: %s on Page %s", str(entity), org.name, str(n))
-    entity.fetch_objects(org, pager=TembaPager(n))
+def fetch_entity(entity, org):
+    logger.info("Fetching Object of type: %s for Org: %s", str(entity), org.name)
+    entity.fetch_objects(org)
 
 
 @task
 def fetch_all(entities=None, orgs=None):
-    print "Started Here"
     if not entities:
-        entities = [dict(name=cls) for cls in BaseUtil.__subclasses__()]
+        entities = BaseUtil.__subclasses__()
     if not orgs:
         orgs = Org.objects.filter(is_active=True)
     else:
@@ -46,14 +43,15 @@ def fetch_all(entities=None, orgs=None):
     for org in orgs:
         for entity in entities:
             try:
-                n = entity.get('start_page', 1)
-                while True:
-                    fetch_entity(entity, org, n)
-                    n += 1
+                fetch_entity(entity, org)
             except TembaException as e:
+                if settings.DEBUG:
+                    raise
                 logger.error("Temba is misbehaving: %s - No retry", str(e))
                 continue
             except Exception as e:
+                if settings.DEBUG:
+                    raise
                 logger.error("Things are dead: %s - No retry", str(traceback.format_exc()))
 
 
