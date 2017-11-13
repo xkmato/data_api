f13638242d860244050d6fd85bfc342010cbedae
Cory Zue <czue@dimagi.com>
2017-11-13 16:29:57 +0200

    extract import_org to a function

--- data_api/api/management/commands/import_org.py
+++ data_api/api/management/commands/import_org.py
@@ -1,6 +1,8 @@
 from django.core.management import BaseCommand
 from temba_client.v2 import TembaClient
 from data_api.api.models import Org
+from data_api.api.utils import import_org
+
 
 class Command(BaseCommand):
 
@@ -13,18 +15,10 @@ class Command(BaseCommand):
             '--server',
             action='store',
             dest='server',
-            default='https://rapidpro.io',
+            default='https://app.rapidpro.io',
             help='Bootstrap an org from an API key',
         )
 
     def handle(self, api_key, server, *args, **options):
         print('server: {}, api key: {}'.format(server, api_key))
-        client = TembaClient(server, api_key)
-        org = client.get_org()
-        org_dict = org.serialize()
-        org_dict['api_token'] = api_key
-        local_org = Org(**org_dict)
-        local_org.save()
-
-
-
+        import_org(server, api_key)
