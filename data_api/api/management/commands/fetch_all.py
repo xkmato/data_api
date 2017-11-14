21ebfacb2fb5a35272c397106a3e06d5e2e5d265
Cory Zue <czue@dimagi.com>
2017-11-14 09:36:27 +0200

    import org in fetch_all, allow passing in server

--- data_api/api/management/commands/fetch_all.py
+++ data_api/api/management/commands/fetch_all.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from data_api.api.tasks import fetch_all
+from data_api.api.utils import import_org
 
 
 class Command(BaseCommand):
@@ -7,6 +8,15 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('api_key')
 
-    def handle(self, api_key, *args, **options):
-        fetch_all(orgs=[api_key])
+        # Named (optional) arguments
+        parser.add_argument(
+            '--server',
+            action='store',
+            dest='server',
+            default='https://app.rapidpro.io',
+            help='Bootstrap an org from an API key',
+        )
 
+    def handle(self, api_key, server, *args, **options):
+        import_org(server, api_key)
+        fetch_all(orgs=[api_key])
