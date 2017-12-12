a1a32cb1fcbed3b44c21a9b255217a46a4913c20
Cory Zue <czue@dimagi.com>
2017-12-12 13:03:06 +0200

    update every day

--- data_api/celery.py
+++ data_api/celery.py
@@ -19,8 +19,9 @@ def setup_periodic_tasks(sender, **kwargs):
         traceback.print_exc()
         sys.exit()
 
-    seconds_in_a_week = 60*60*24*7
-    update_interval = int(os.environ.get('FETCH_INTERVAL', seconds_in_a_week))
+    # seconds_in_a_week = 60*60*24*7
+    seconds_in_a_day = 60*60*24
+    update_interval = int(os.environ.get('FETCH_INTERVAL', seconds_in_a_day))
     sender.add_periodic_task(update_interval, sync_latest_data.s(),
                              name='Sync data from RapidPRO every {} seconds'.format(update_interval))
 
