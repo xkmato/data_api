d33030bfac53ce735275089953db7034cdac0bed
Cory Zue <czue@dimagi.com>
2018-06-11 13:18:08 +0200

    start improving admin

--- data_api/staging/admin.py
+++ data_api/staging/admin.py
@@ -2,9 +2,28 @@ from django.contrib import admin
 
 from data_api.staging import models
 
-admin.site.register(models.Organization)
-admin.site.register(models.Group)
-admin.site.register(models.Contact)
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'is_active', 'server', 'api_token']
+    list_filter = ['is_active', 'server']
+
+
+ORG_MODEL_FIELDS = ['organization', 'first_synced', 'last_synced']
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'uuid'] + ORG_MODEL_FIELDS
+    list_filter = ORG_MODEL_FIELDS
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'uuid'] + ORG_MODEL_FIELDS
+    list_filter = ORG_MODEL_FIELDS
+
+
+admin.site.register(models.Organization, OrganizationAdmin)
+admin.site.register(models.Group, GroupAdmin)
+admin.site.register(models.Contact, ContactAdmin)
 admin.site.register(models.Field)
 admin.site.register(models.Channel)
 admin.site.register(models.ChannelEvent)
