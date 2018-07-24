92804ab14bd7f3fe7bc1d8b93d59c686ea67d911
Cory Zue <czue@dimagi.com>
2018-07-24 16:22:13 +0200

    add --noinput option to delete_organization

--- data_api/staging/management/commands/delete_organization.py
+++ data_api/staging/management/commands/delete_organization.py
@@ -7,6 +7,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional arguments
         parser.add_argument('org_id')
+        parser.add_argument(
+            '--noinput',
+            action='store_true',
+            dest='noinput',
+            default=False,
+            help='No prompts will be shown to user.',
+        )
 
     def handle(self, org_id, *args, **options):
 
@@ -16,13 +23,18 @@ class Command(BaseCommand):
             # try by api token
             org = Organization.objects.get(api_token=org_id)
         org_models = OrganizationModel.__subclasses__()
-        print('Are you sure you want to delete this organization: {} ({}) including the following models?'.format(
-            org.name, org.id
-        ))
+        noinput = options['noinput']
+        if noinput:
+            print('Deleting the following models:')
+        else:
+            print('Are you sure you want to delete this organization: {} ({}) including the following models?'.format(
+                org.name, org.id
+            ))
 
         for model in org_models:
             print('  {} {}s'.format(model.objects.filter(organization=org).count(), model.__name__))
-        if raw_input('y/N: ').lower().strip() == 'y':
+
+        if noinput or raw_input('y/N: ').lower().strip() == 'y':
             org.delete()
             print('org {} was deleted.'.format(org.name))
         else:
