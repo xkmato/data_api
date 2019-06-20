c7d8b4c1ad68732383d9f9f6b9aa074a1856d07a
Cory Zue <czue@dimagi.com>
2019-06-20 10:18:05 +0200

    delete unused functions

--- data_api/api/utils.py
+++ data_api/api/utils.py
@@ -1,24 +1,7 @@
-from datetime import datetime
-import os
-from django.conf import settings
 from mongoengine import DoesNotExist
 from temba_client.v2 import TembaClient
 
 __author__ = 'kenneth'
-
-
-def get_date_from_param(param):
-    if len(param) != 8:
-        raise RuntimeError("Wrong date format. Use: ddmmyyyy")
-    return datetime(int(param[4:8]), int(param[2:4]), int(param[:2]))
-
-
-def create_folder_for_org(org_id):
-    path = '%s/%s' % (settings.CSV_DUMPS_FOLDER, org_id)
-    if not os.path.exists(path):
-        os.makedirs(path)
-        os.makedirs('%s/messages' % path)
-        os.makedirs('%s/runs' % path)
 
 
 def import_org(server, api_key):
